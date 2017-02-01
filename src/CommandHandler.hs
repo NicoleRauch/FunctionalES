f3ea2d4019d306cd5736a279283ceb83c875d219
Nicole Rauch <nicole.m@gmx.de>
2017-02-01 12:25:46 +0100

    Added a command handler

--- src/CommandHandler.hs
+++ src/CommandHandler.hs
@@ -1,4 +1,24 @@
 module CommandHandler where
 
+import Data.IORef
 
 
+handleCommand :: Command -> EventStore -> IO [Event]
+handleCommand command eventStore = do
+  pastEvents <- readEvents eventStore
+  let state = foldl evolve initialState pastEvents
+  let newEvents = decide command state
+  appendEvents eventStore newEvents
+  return newEvents
+
+
+data EventStore = Store (IORef [Event])
+
+newEventStore :: IO EventStore
+newEventStore = fmap Store $ newIORef []
+
+readEvents :: EventStore -> IO [Event]
+readEvents (Store ref) = readIORef ref
+
+appendEvents :: EventStore -> [Event] -> IO ()
+appendEvents (Store ref) events = modifyIORef ref (++ events)
